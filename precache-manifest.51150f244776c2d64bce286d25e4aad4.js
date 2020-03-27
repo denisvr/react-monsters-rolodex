3ba2ac418957a0c2a0ad45880ff9393bb303a526
denisvr <denisvr01@gmail.com>
2020-03-27 19:50:17 +0000

    Updates

---
+++ precache-manifest.51150f244776c2d64bce286d25e4aad4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa321ec7a7fc076c78257c5f3fcaf03e",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "687bd9830bc31fab6591",
+    "url": "/react-monsters-rolodex/static/css/main.ada67250.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react-monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "687bd9830bc31fab6591",
+    "url": "/react-monsters-rolodex/static/js/main.9e9534f7.chunk.js"
+  },
+  {
+    "revision": "84d462a1ad20fdb403f4",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.6eda5f3f.js"
+  }
+]);
